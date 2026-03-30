7d668b8680f4038835eb7894b4e93ba3768508d8
Rudra Pratap Dhakad <rudradhakad@12304@gmail.com>
2026-03-30 20:48:56 +0530

    auth.go

--- internal/auth/model/auth.go
+++ internal/auth/model/auth.go
@@ -1,1 +1,35 @@
 package model
+
+import "time"
+
+type User struct {
+	UserID       string    `db:"user_id"`
+	Email        string    `db:"email"`
+	PasswordHash string    `db:"password_hash"`
+	Role         string    `db:"role"`
+	RefreshToken string    `db:"refresh_token"`
+	IsActive     bool      `db:"is_active"`
+	CreatedAt    time.Time `db:"created_at"`
+}
+
+type RegisterRequest struct {
+	Email    string `json:"email"    binding:"required,email"`
+	Password string `json:"password" binding:"required,min=8"`
+	Role     string `json:"role"     binding:"required,oneof=founder vc"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email"    binding:"required,email"`
+	Password string `json:"password" binding:"required"`
+}
+
+type AuthResponse struct {
+	UserID       string `json:"user_id"`
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+	Role         string `json:"role"`
+}
+
+type RefreshRequest struct {
+	RefreshToken string `json:"refresh_token" binding:"required"`
+}
